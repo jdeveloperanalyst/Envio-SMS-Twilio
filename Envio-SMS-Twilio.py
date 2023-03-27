4162ca904f7e07d912501ccacc21d34e6643a242
jdeveloperanalyst <jonatas1200@icloud.com>
2023-03-27 18:19:19 -0300

    Leitura das bases de dados no formato excel

--- Envio-SMS-Twilio.py
+++ Envio-SMS-Twilio.py
@@ -1,1 +1,10 @@
-print('iniciando projeto')
+import pandas as pd
+
+# Criando uma lista com o nome dos primeiros 6 meses do ano.
+months = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho']
+
+# Loop FOR fazendo a leitura das bases de dados no formato excel mediante a cada mês que está sendo analisado
+for month in months:
+    print(month)
+    sales_table = pd.read_excel(fr'C:\Users\jonat\Documents\Meus Projetos\Python\Projeto 4 Envio SMS Twilio\Envio-SMS-Twilio\Relatório de Vendas\{month}.xlsx')
+    print(sales_table)
