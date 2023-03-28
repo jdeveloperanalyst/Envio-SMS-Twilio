9b93b7271553c9cd8295e1bde7219bf09af5bf19
jdeveloperanalyst <jonatas1200@icloud.com>
2023-03-28 19:19:54 -0300

    fazendo conexão com o client que é meu perfil criado no site do twilio.

--- Envio-SMS-Twilio.py
+++ Envio-SMS-Twilio.py
@@ -1,6 +1,12 @@
 import pandas as pd
 from twilio.rest import Client
 
+# Criando variáveis no qual se refere ao SID(usuário) e um token(autenticação) criado no site twilio.com/console para que seja possível uma conexão ao twilio para envio da menssagem
+account_sid = 'AC369c3144069b317371e51514ca03763d'
+auth_token = 'a9837411cf75d8463a8bedd0e8c6bbaf'
+client = Client(account_sid, auth_token)
+
+
 # Criando uma lista com o nome dos primeiros 6 meses do ano.
 months = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho']
 
@@ -11,4 +17,3 @@ for month in months:
         seller = sales_table.loc[(sales_table['Vendas'] > 55000), 'Vendedor'].values[0]
         sale = sales_table.loc[(sales_table['Vendas'] > 55000), 'Vendas'].values[0]
         print(f'No mês de {month}, o vendedor {seller} bateu a meta com {sale} vendas!')
-
