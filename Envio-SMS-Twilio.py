1d6b6748c338e3f57ad10c0fcc26d4d4e1dc2328
jdeveloperanalyst <jonatas1200@icloud.com>
2023-03-28 20:04:52 -0300

    Add git ignore

--- Envio-SMS-Twilio.py
+++ Envio-SMS-Twilio.py
@@ -3,7 +3,7 @@ from twilio.rest import Client
 
 # Criando variáveis no qual se refere ao SID(usuário) e um token(autenticação) criado no site twilio.com/console para que seja possível uma conexão ao twilio para envio da menssagem
 account_sid = 'AC369c3144069b317371e51514ca03763d'
-auth_token = 'a9837411cf75d8463a8bedd0e8c6bbaf'
+auth_token = '2c97b8f5f7ce2c1fb4fe6e2f463b88de'
 client = Client(account_sid, auth_token)
 
 
@@ -17,3 +17,9 @@ for month in months:
         seller = sales_table.loc[(sales_table['Vendas'] > 55000), 'Vendedor'].values[0]
         sale = sales_table.loc[(sales_table['Vendas'] > 55000), 'Vendas'].values[0]
         print(f'No mês de {month}, o vendedor {seller} bateu a meta com {sale} vendas!')
+        # Envio da mensagem
+        message = client.messages.create(
+            to="+5511966489321",
+            from_="+14346239362",
+            body=f'No mês de {month}, o vendedor {seller} bateu a meta com {sale} vendas!')
+        print(message.sid)
