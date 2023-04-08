867f9955013593ce692ab748fa83097ca84714eb
jdeveloperanalyst <jonatas1200@icloud.com>
2023-04-08 19:37:50 -0300

    subindo alterações sobre a biblioteca time sleep.

--- Envio-SMS-Twilio.py
+++ Envio-SMS-Twilio.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from twilio.rest import Client
 from Credenciais import secret
+from time import sleep
 
 # Criando variáveis no qual se refere ao SID(usuário) e um token(autenticação) criado no site twilio.com/console para que seja possível uma conexão ao twilio para envio da menssagem
 account_sid = secret['sid']
@@ -17,9 +18,11 @@ for month in months:
         seller = sales_table.loc[(sales_table['Vendas'] > 55000), 'Vendedor'].values[0]
         sale = sales_table.loc[(sales_table['Vendas'] > 55000), 'Vendas'].values[0]
         print(f'No mês de {month}, o vendedor {seller} bateu a meta com {sale} vendas!')
+        sleep(3)
         # Envio do SMS
         sms = client.messages.create(
             to=secret['my_cellphone'],
             from_=secret['twilio_phone'],
             body=f'No mês de {month}, o vendedor {seller} bateu a meta com {sale} vendas!')
+        sleep(3)
         print(sms.sid)
